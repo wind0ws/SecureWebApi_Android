dbea7e2c68752b28f78beb66e2eb8ca06e1e3f38
Threshold <hsjfox@foxmail.com>
2016-02-17 09:39:52 +0800

    update lib version

--- app/src/main/java/threshold/securewebapi/api/ApiService.java
+++ app/src/main/java/threshold/securewebapi/api/ApiService.java
@@ -6,9 +6,11 @@ import com.google.gson.GsonBuilder;
 
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
-import retrofit2.GsonConverterFactory;
+//import retrofit2.GsonConverterFactory;
 import retrofit2.Retrofit;
-import retrofit2.RxJavaCallAdapterFactory;
+//import retrofit2.RxJavaCallAdapterFactory;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
 import threshold.securewebapi.BuildConfig;
 import threshold.securewebapi.Constant;
 import threshold.webapiauth.SecureRequestInterpolator;
